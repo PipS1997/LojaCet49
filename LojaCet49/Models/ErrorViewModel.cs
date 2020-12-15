ef261e95487202d6bfe8b1e0783ebd7ce749a87a
PipS <PipS@DESKTOP-FRMKOBB>
2020-12-15 15:36:49 +0000

    Inicialização do projecto

---
+++ LojaCet49/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace LojaCet49.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
