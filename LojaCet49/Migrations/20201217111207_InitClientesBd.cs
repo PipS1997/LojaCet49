614947646c7a96810fea191cee54494ed2dccb23
PipS <PipS@DESKTOP-FRMKOBB>
2020-12-17 11:18:38 +0000

    Clientes base de dados web

---
+++ LojaCet49/Migrations/20201217111207_InitClientesBd.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace LojaCet49.Migrations
+{
+    public partial class InitClientesBd : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Clientes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Nome = table.Column<string>(nullable: true),
+                    Morada = table.Column<string>(nullable: true),
+                    Telemóvel = table.Column<string>(nullable: true),
+                    Email = table.Column<string>(nullable: true),
+                    DNasc = table.Column<DateTime>(nullable: false),
+                    NIF = table.Column<int>(nullable: false),
+                    UltimaCompra = table.Column<DateTime>(nullable: false),
+                    ValorDivida = table.Column<double>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Clientes", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Clientes");
+        }
+    }
+}
